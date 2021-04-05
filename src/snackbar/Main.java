4eedd21353aa121ff43025d4a4e751874efe9177
Beau Jennings <redalthazar@gmail.com>
2021-04-05 16:16:37 -0500

    built Main.java and Snack.java

---
+++ src/snackbar/Main.java
@@ -0,0 +1,14 @@
+package snackbar;
+
+public class Main 
+    {
+        public static void main(String[] args) 
+        {
+            workWithData();
+        }
+
+        private static void workWithData()
+        {
+            
+        }
+    }
---
+++ src/snackbar/Snack.java
@@ -0,0 +1,12 @@
+package snackbar;
+
+public class Snack 
+{
+    private int id;
+    private static int maxId = 1;
+    private int vendingMachineId;
+
+    private String name;
+    private int quantity;
+    private double cost;
+}
