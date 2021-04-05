3f969647109bcac85a8322c36a78d1867581760b
Beau Jennings <redalthazar@gmail.com>
2021-04-05 16:24:21 -0500

    built VendingMachine.java

--- src/snackbar/Snack.java
+++ src/snackbar/Snack.java
@@ -9,4 +9,69 @@ public class Snack
     private String name;
     private int quantity;
     private double cost;
+
+
+    public Snack (int vendingMachineId, String name, int quantity, double cost)
+    {
+        id = maxId++;
+        this.vendingMachineId = vendingMachineId;
+        this.name = name;
+        this.quantity = quantity;
+        this.cost = cost;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public int getVendingMachineId()
+    {
+        return vendingMachineId;
+    }
+
+    public void setVendingMachineId(int vendingMachineId)
+    {
+        this.vendingMachineId = vendingMachineId;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    public void setAddQuantity()
+    {
+        this.quantity = quantity - this.quantity;
+    }
+
+    public void setBuySnack(int quantity)
+    {
+        this.quantity = this.quantity - quantity;
+    }
+
+    public double getCost()
+    {
+        return cost;
+    }
+
+    public void setCost(double cost)
+    {
+        this.cost = cost;
+    }
+
+    public void setTotalCost(double cost, int quantity)
+    {
+        this.cost = cost * quantity;
+    }
 }
---
+++ src/snackbar/VendingMachine.java
@@ -0,0 +1,15 @@
+package snackbar;
+
+public class VendingMachine 
+{
+    private static int maxId = 0;
+    public int id;
+    public String name;
+    
+    public VendingMachine(String name)
+    {
+        maxId++;
+        id = maxId;
+        this.name = name;
+    }
+}
